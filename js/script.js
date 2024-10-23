73adb1844ef321b68c528d868a166e80821c2fb7
Mehedi Hasan <117835267+MhXton@users.noreply.github.com>
2024-10-23 23:48:35 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.logo-area').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 529,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
